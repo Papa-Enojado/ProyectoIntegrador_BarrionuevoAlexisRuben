33f45856c4bcac754601365c680164f23bbdecc4
Papa-Enojado <alex.polara@gmail.com>
2023-05-12 20:43:11 -0300

    se agrega boton y retoques

--- ProyectoIntegrador_BarrionuevoAlexisRuben/src/app/app.module.ts
+++ ProyectoIntegrador_BarrionuevoAlexisRuben/src/app/app.module.ts
@@ -5,27 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
-import { RsocialComponent } from './components/rsocial/rsocial.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
 import  {  NgCircleProgressModule  }  from  'ng-circle-progress';
 import { HabilidadesComponent } from './components/habilidades/habilidades.component';
-import { ProyectoComponent } from './components/proyecto/proyecto.component' ;
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component' ;
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    RsocialComponent,
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
     HabilidadesComponent,
-    ProyectoComponent
+    ProyectoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
